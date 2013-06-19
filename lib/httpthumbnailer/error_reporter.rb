ea3a48a4d75d6e56a50a40642749ca3d00abeb01
WhatClinic.com <wcc@lucy.sigquit.net>
2013-06-19 16:03:21 +0200

    using yielded error

--- lib/httpthumbnailer/error_reporter.rb
+++ lib/httpthumbnailer/error_reporter.rb
@@ -1,37 +1,37 @@
 class ErrorReporter < Controler
 	self.define do
-		on error Rack::UnhandledRequest::UnhandledRequestError do
-			write_error 404, env['app.error']
+		on error Rack::UnhandledRequest::UnhandledRequestError do |error|
+			write_error 404, error
 		end
 
-		on error Plugin::Thumbnailer::UnsupportedMediaTypeError do
-			write_error 415, env['app.error']
+		on error Plugin::Thumbnailer::UnsupportedMediaTypeError do |error|
+			write_error 415, error
 		end
 
 		on error(
 			Plugin::Thumbnailer::ImageTooLargeError,
 			MemoryLimit::MemoryLimitedExceededError
-		)	do
-			write_error 413, env['app.error']
+		)	do |error|
+			write_error 413, error
 		end
 
 		on error(
 			ThumbnailSpec::BadThubnailSpecError,
 			Plugin::Thumbnailer::ZeroSizedImageError,
 			Plugin::Thumbnailer::UnsupportedMethodError
-		) do
-			write_error 400, env['app.error']
+		) do |error|
+			write_error 400, error
 		end
 
-		log.error "unhandled error while processing request: #{env['REQUEST_METHOD']} #{env['SCRIPT_NAME']}[#{env["PATH_INFO"]}]", env['app.error']
-		log.debug {
-			out = StringIO.new
-			PP::pp(env, out, 200)
-			"Request: \n" + out.string
-		}
+		on error StandardError do |error|
+			log.error "unhandled error while processing request: #{env['REQUEST_METHOD']} #{env['SCRIPT_NAME']}[#{env["PATH_INFO"]}]", error
+			log.debug {
+				out = StringIO.new
+				PP::pp(env, out, 200)
+				"Request: \n" + out.string
+			}
 
-		on error StandardError do
-			write_error 500, env['app.error']
+			write_error 500, error
 		end
 	end
 end
