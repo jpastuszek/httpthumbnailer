29b0e6d0805c9b8d6bb0b7e0ce8aff03bae9f8d3
Jakub Pastuszek <jpastuszek@gmail.com>
2013-08-13 15:46:09 +0200

    using default error reporter from base; added identity test to soak test and results

--- lib/httpthumbnailer/error_reporter.rb
+++ lib/httpthumbnailer/error_reporter.rb
@@ -1,9 +1,5 @@
 class ErrorReporter < Controler
 	self.define do
-		on error Rack::UnhandledRequest::UnhandledRequestError do |error|
-			write_error 404, error
-		end
-
 		on error Plugin::Thumbnailer::UnsupportedMediaTypeError do |error|
 			write_error 415, error
 		end
@@ -23,16 +19,7 @@ class ErrorReporter < Controler
 			write_error 400, error
 		end
 
-		on error StandardError do |error|
-			log.error "unhandled error while processing request: #{env['REQUEST_METHOD']} #{env['SCRIPT_NAME']}[#{env["PATH_INFO"]}]", error
-			log.debug {
-				out = StringIO.new
-				PP::pp(env, out, 200)
-				"Request: \n" + out.string
-			}
-
-			write_error 500, error
-		end
+		run DefaultErrorReporter
 	end
 end
 
